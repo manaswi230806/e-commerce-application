0e089559a3ec3544907c3da2c70d9f7f54022ec4
Jasthi Manaswi <jasthimanaswi@gmail.com>
2026-05-25 21:54:50 +0530

    fixed cors for vercel frontend

--- server/server.js
+++ server/server.js
@@ -8,7 +8,16 @@ const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://e-commerce-application-eebdy106c-manaswi230806s-projects.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
