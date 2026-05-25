567eb5bbf8294f9702d64967ce9483858f1cdf58
Jasthi Manaswi <jasthimanaswi@gmail.com>
2026-05-25 22:17:15 +0530

    fixed cors final

--- server/server.js
+++ server/server.js
@@ -8,29 +8,36 @@ const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://e-commerce-application-eebdy106c-manaswi230806s-projects.vercel.app",
+      "https://e-commerce-application-j1v8zw9jc-manaswi230806s-projects.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
-app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+// Default Route
 app.get("/", (req, res) => {
-  res.send("API Running");
+  res.send("E-Commerce Backend Running");
 });
 
+// Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
